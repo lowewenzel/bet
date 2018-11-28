cc536b5d44026099a28959f2754fb2b88671c2b9
wenzel <lenzelwowe@gmail.com>
2018-11-27 16:37:47 -0800

    add express static fix

--- backend/config/express.js
+++ backend/config/express.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static('public'));
 
 
 app.use(cookieParser(config.cookieSecret));
